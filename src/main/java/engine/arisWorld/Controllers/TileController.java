6725356829408697f88acdedf19ab5c8f2fd8cab
gshiporin <gshiporin@gmail.com>
2021-01-16 14:43:28 +0300

    +GetTilesAround

--- src/main/java/engine/arisWorld/Controllers/TileController.java
+++ src/main/java/engine/arisWorld/Controllers/TileController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 
 @RestController
 public class TileController {
@@ -29,6 +30,21 @@ public class TileController {
     public Object makeTile (@Valid @RequestBody Tile tile){
         tile = tileRepository.save(tile);
         return tile;
+    }
+
+    @GetMapping(path = "/api/getTilesAroundByCoordinates/{coordinates_x},{coordinates_y}", produces = "application/json")
+    public Object getTilesAroundByCoordinates(@PathVariable int coordinates_x, @PathVariable int coordinates_y){
+        ArrayList<Tile> tiles = new ArrayList<>();
+        int[] x = {-1,0,1};
+        int[] y = {-1,0,1};
+
+        for (int i: x) {
+            for (int j:y) {
+                tiles.add(tileRepository.findById(new TileId(coordinates_x+i,coordinates_y+j))
+                        .orElse(new Tile(new TileId(coordinates_x+i,coordinates_y+j), "NoTile", null)));
+            }
+        }
+        return tiles;
     }
 
 }
